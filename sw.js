ce8ec4d99167e770892b1a6a4e71fe376c820247
english-fun-game <pong201217@gmail.com>
2026-08-04 18:29:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'pongcrush-v2';
+const CACHE_NAME = 'pongcrush-v3';
 
-// 캐싱할 정적, 미디어 자원 및 엑셀 파일 리스트
+// 캐싱할 정적, 미디어 자원 및 엑셀 파일 리스트 (고양이 이미지 삭제됨)
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -14,12 +14,7 @@ const ASSETS_TO_CACHE = [
     'https://docs.google.com/spreadsheets/d/1VCSxOUY7bDuZlp3JtRPANYFXxBAzFBxrYCj6tC89n1U/export?format=xlsx',
 
     // 오디오 파일 (1~8.mp3)
-    ...Array.from({ length: 8 }, (_, i) => `./sounds/${i + 1}.mp3`),
-    
-    // 이미지 파일 (type1, type2, type3 / 1~4.jpg)
-    ...['type1', 'type2', 'type3'].flatMap(type => 
-        Array.from({ length: 4 }, (_, i) => `./images/${type}/${i + 1}.jpg`)
-    )
+    ...Array.from({ length: 8 }, (_, i) => `./sounds/${i + 1}.mp3`)
 ];
 
 // 서비스 워커 설치 및 리소스 캐싱
